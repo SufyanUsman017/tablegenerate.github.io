f49b34832f721a2a91bf971a4f9f65fed11bdb3c
SufyanUsman017 <162179435+SufyanUsman017@users.noreply.github.com>
2024-11-14 12:57:34 +0500

    Table-Generate

---
+++ tab.js
@@ -0,0 +1,98 @@
+
+// ****************simple method multipli div sub add***********************
+a = 5;
+b = 20;
+c = a+b;
+console.log('Enter the value of adding',c);
+
+a = 5;
+b = 20;
+c = a-b;
+console.log('Enter the value of sub',c);
+
+a = 5;
+b = 20;
+c = a*b;
+console.log('Enter the value of mult',c);
+
+console.log('Hy how are you',"i am fine");
+
+// ****************simple method multipli div sub add***********************
+
+// ****************name variable***********************
+
+std = "sufyan"
+
+console.log(`${std} is a good boy`, `${std} is a telented person in the world` )
+
+// ****************table start***********************
+
+tab = 2;
+
+document.write(`${tab} x 1 =  ${tab*1} <br>`)
+document.write(`${tab} x 2 =  ${tab*2} <br>`)
+document.write(`${tab} x 3 =  ${tab*3} <br>`)
+document.write(`${tab} x 4 =  ${tab*4} <br>`)
+document.write(`${tab} x 5 =  ${tab*5} <br>`)
+document.write(`${tab} x 6 =  ${tab*6} <br>`)
+document.write(`${tab} x 7 =  ${tab*7} <br>`)
+document.write(`${tab} x 8 =  ${tab*8} <br>`)
+document.write(`${tab} x 9 =  ${tab*9} <br>`)
+document.write(`${tab} x 10 =  ${tab*10} <br>`)
+
+
+console.log(`${tab} x 1 = ${tab*1 } `)
+console.log(`${tab} x 2 = ${tab*2 } `)
+console.log(`${tab} x 3 = ${tab*3 } `)
+console.log(`${tab} x 4 = ${tab*4 } `)
+console.log(`${tab} x 5 = ${tab*5 } `)
+console.log(`${tab} x 6 = ${tab*6 } `)
+console.log(`${tab} x 7 = ${tab*7 } `)
+console.log(`${tab} x 8 = ${tab*8 } `)
+console.log(`${tab} x 9 = ${tab*9 } `)
+console.log(`${tab} x 10 = ${tab*10}`)
+
+// ****************table end***********************
+
+// ****************prompt value***********************
+
+console.log(prompt("Enter your name"))
+
+a = prompt("enter value of a");
+b =  prompt("enter value of b");
+
+console.log("Answer of multiplication ",a*b);
+console.log("Answer of division ",a/b);
+console.log("Answer of substraction ",a-b);
+
+
+// ****************var let const***********************
+
+// var a = 2;
+// var a = 4;
+
+// let b = 2;
+// b = 4;
+
+// const c = 2;
+// c = 5;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+const g = 5;
+console.log(100*g);
+
+// ****************var let const***********************
+// ****************typeof***********************
+a = 10;
+console.log(typeof(a));
+
+num1 = prompt("Enter 1st Number");
+console.log(2 + num1)
+// ****************typeof end***********************
+
+ 
+
+
